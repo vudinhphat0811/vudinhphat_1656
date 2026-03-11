d203280f1693f57430ca2b3da19f0827b0452edd
vudinhphat0811 <vphat544@gmail.com>
2026-03-11 11:03:17 +0700

    add

--- lab02/api.py
+++ lab02/api.py
@@ -2,9 +2,10 @@ from cipher.vigenere import VigenereCipher
 from flask import Flask, request, jsonify
 from cipher.railfence.railfence_cipher import RailFenceCipher
 from cipher.playfair.playfair_cipher import PlayfairCipher
+from cipher.transposition.transposition_cipher import TranspositionCipher
 app = Flask(__name__)
 vigenere_cipher = VigenereCipher()
-
+transposition_cipher = TranspositionCipher()
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
     data = request.json
@@ -89,6 +90,22 @@ def playfair_decrypt():
     key = data['key']
     playfair_matrix = playfair_cipher.create_playfair_matrix(key)
     decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_text': decrypted_text})
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
 if __name__ == "__main__":
